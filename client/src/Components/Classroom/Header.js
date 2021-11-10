2c15e78633e2131d01cf87c47be4a0b06bfeb863
nitika2000 <nitikagupta208@gmail.com>
2021-11-11 00:38:13 +0530

    basic form model added

--- client/src/Components/Classroom/Header.js
+++ client/src/Components/Classroom/Header.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { useState } from "react";
+import JoinClass from "./JoinClass";
 
 function Header() {
+  const [showJoinForm, setshowJoinForm] = useState(false);
+
   return (
     <div className="p-4">
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      <button
+        onClick={() => {
+          setshowJoinForm(true);
+        }}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
         Join classroom
       </button>
+
+      {showJoinForm ? (
+        <JoinClass closeForm={() => setshowJoinForm(false)} />
+      ) : null}
     </div>
   );
 }
