ce539c85c4d8b2d4b3b00afaf4bab78cabef8a86
nitika2000 <nitikagupta208@gmail.com>
2021-11-10 11:59:32 +0530

    basic classroom-page setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import "./styles/output.css";
 import Nav from "./Components/navbar";
+import JoinedClasses from "./Components/Classroom/JoinedClasses";
+import { ClassList } from "./data";
 
 const App = () => {
   return (
     <>
     <Nav/>
+    <JoinedClasses classList = {ClassList}/>
     </>
   );
 };
---
+++ client/src/Components/Classroom/JoinedClasses.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ClassCard from './ClassCard';
+
+
+
+function JoinedClasses(props) {
+    return (
+            <div className="flex flex-wrap content-evenly gap-4">
+                {
+                    props.classList.map((classroom, key) => {
+                        return (<ClassCard key={key} classroom={classroom} />)
+                    })
+                }
+            </div>
+    );
+};
+
+export default JoinedClasses;
