ef8709643506ad3ed206fa473804239ea6c81c40
nitika2000 <nitikagupta208@gmail.com>
2021-11-11 13:57:24 +0530

    added getclasses from firebase

--- client/src/Components/Classroom/JoinedClasses.js
+++ client/src/Components/Classroom/JoinedClasses.js
@@ -1,8 +1,22 @@
 import React from "react";
 import ClassCard from "./ClassCard";
 import db from "../../services/firebase-config";
+import { collection, query, onSnapshot } from "firebase/firestore";
+
+async function getClasses() {
+  const q = query(collection(db, "classrooms"));
+  const unsubscribe = onSnapshot(q, (querySnapshot) => {
+    const classes = [];
+    querySnapshot.forEach((doc) => {
+      classes.push(doc.data().name);
+    });
+    console.log(classes.join(", "));
+  });
+}
 
 function JoinedClasses({ classes }) {
+  getClasses();
+
   return (
     <div className="p-4 flex flex-wrap content-evenly gap-4">
       {classes.map((classroom, key) => {
--- client/src/services/firebase-config.js
+++ client/src/services/firebase-config.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore/lite";
+import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
+const firebaseApp = initializeApp({
   apiKey: "AIzaSyDLOqukbn-gcLUSe4rJuTfJj-ONQblJ3pE",
   authDomain: "ms-engage-11f78.firebaseapp.com",
   projectId: "ms-engage-11f78",
@@ -9,9 +9,7 @@ const firebaseConfig = {
   messagingSenderId: "623195073013",
   appId: "1:623195073013:web:c9647f36a8ada10ab898bc",
   measurementId: "G-CG4QHTEZ86",
-};
+});
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
+const db = getFirestore(firebaseApp);
 export default db;
