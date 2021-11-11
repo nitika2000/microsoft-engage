c33bed9f844ce2780e08b80f66f6602fe4818a06
nitika2000 <nitikagupta208@gmail.com>
2021-11-11 15:06:56 +0530

    basic create class form added

--- client/src/Components/Classroom/Header.js
+++ client/src/Components/Classroom/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { useState } from "react";
+import CreateClass from "./CreateClass";
 import JoinClass from "./JoinClass";
 
 function Header() {
   const [showJoinForm, setshowJoinForm] = useState(false);
+  const [showCreateForm, setshowCreateForm] = useState(false);
 
   const joinClass = (classCode) => {
     console.log(classCode);
@@ -21,11 +23,24 @@ function Header() {
         Join classroom
       </button>
 
+      <button
+        onClick={() => {
+          setshowCreateForm(true);
+        }}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Create classroom
+      </button>
+
       {showJoinForm ? (
         <JoinClass
           joinClass={joinClass}
           closeForm={() => setshowJoinForm(false)}
         />
+      ) : null}
+
+      {showCreateForm ? (
+        <CreateClass closeForm={() => setshowCreateForm(false)} />
       ) : null}
     </div>
   );
