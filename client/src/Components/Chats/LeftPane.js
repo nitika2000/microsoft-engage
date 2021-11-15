83eaff5bf1c30f6862404c3a578cb6adc2eccc4a
nitika2000 <nitikagupta208@gmail.com>
2021-11-15 13:47:59 +0530

    userlist refactored

--- client/src/Components/Chats/LeftPane.js
+++ client/src/Components/Chats/LeftPane.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { useAuth } from "../AuthContext";
 import UserCard from "./UserCard";
+import { useState, useEffect } from "react";
+import { collection, where, query, onSnapshot } from "@firebase/firestore";
+import db from "../../services/firebase-config";
 
-function LeftPane({ onSelect, usersList, loading }) {
+function LeftPane({ onSelect }) {
+  const { currentUser } = useAuth();
+  const [usersList, setUsersList] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const usersRef = collection(db, "users");
+    const q = query(usersRef, where("uid", "not-in", [currentUser.uid]));
+
+    const unsub = onSnapshot(q, (querySnapshot) => {
+      let users = [];
+      querySnapshot.forEach((doc) => {
+        users.push(doc.data());
+      });
+      setUsersList(users);
+      setLoading(false);
+    });
+    return () => {
+      unsub();
+    };
+  }, []);
+
   return (
     <>
       {loading ? (
--- client/src/pages/ChatPage.js
+++ client/src/pages/ChatPage.js
@@ -1,33 +1,10 @@
 import React from "react";
-import { useAuth } from "../Components/AuthContext";
-import { useState, useEffect } from "react";
-import { collection, where, query, onSnapshot } from "@firebase/firestore";
-import db from "../services/firebase-config";
+import { useState } from "react";
 import LeftPane from "../Components/Chats/LeftPane";
 import ChatView from "../Components/Chats/ChatView";
 
 function ChatPage() {
-  const { currentUser } = useAuth();
-  const [usersList, setUsersList] = useState([]);
-  const [leftPaneLoading, setLeftPaneLoading] = useState(true);
   const [selectedUser, setSelectedUser] = useState();
-
-  useEffect(() => {
-    const usersRef = collection(db, "users");
-    const q = query(usersRef, where("uid", "not-in", [currentUser.uid]));
-
-    const unsub = onSnapshot(q, (querySnapshot) => {
-      let users = [];
-      querySnapshot.forEach((doc) => {
-        users.push(doc.data());
-      });
-      setUsersList(users);
-      setLeftPaneLoading(false);
-    });
-    return () => {
-      unsub();
-    };
-  }, []);
 
   const onUserSelect = (user) => {
     setSelectedUser(user);
@@ -35,11 +12,7 @@ function ChatPage() {
 
   return (
     <div className="flex h-[80vh] inset-0 container mx-auto shadow-md border p-4 gap-4 mt-8 divide-x-2 ">
-      <LeftPane
-        onSelect={onUserSelect}
-        usersList={usersList}
-        loading={leftPaneLoading}
-      />
+      <LeftPane onSelect={onUserSelect} />
       <ChatView selectedUser={selectedUser} />
     </div>
   );
