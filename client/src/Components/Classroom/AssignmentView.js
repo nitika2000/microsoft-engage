841e857cc5af792076e374af4d9ceef6c67d7bc5
nitika2000 <nitikagupta208@gmail.com>
2021-11-15 22:36:36 +0530

    basic assignment view setup

--- client/src/Components/Classroom/AssignmentView.js
+++ client/src/Components/Classroom/AssignmentView.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import { doc, getDoc } from "@firebase/firestore";
+import db from "../../services/firebase-config";
 
 function AssignmentView() {
   const searchParams = useParams();
   const classId = searchParams.classId;
   const assignId = searchParams.assignId;
-  return (
-    <div>
-      this is assignment view for {classId} {assignId}
-    </div>
+  const [assignment, setAssignment] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const assignRef = doc(db, "classPosts", classId, "assignments", assignId);
+    const docSnap = getDoc(assignRef).then((assign) => {
+      setAssignment(assign.data());
+      setLoading(false);
+    });
+  }, []);
+
+  return loading ? (
+    <h1>Loading</h1>
+  ) : assignment ? (
+    <div>this is assignment view for {assignment.description}</div>
+  ) : (
+    <h1>Url is incorrent : 404</h1>
   );
 }
 
