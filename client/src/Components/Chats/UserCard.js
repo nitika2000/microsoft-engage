fdf1747826d43ca17af809d7d5726367c2f824a4
nitika2000 <nitikagupta208@gmail.com>
2021-11-15 14:26:43 +0530

    bold font to unread messages

--- client/src/Components/Chats/UserCard.js
+++ client/src/Components/Chats/UserCard.js
@@ -1,4 +1,4 @@
-import { collection, onSnapshot, doc } from "@firebase/firestore";
+import { onSnapshot, doc } from "@firebase/firestore";
 import React, { useState, useEffect } from "react";
 import db from "../../services/firebase-config";
 import { getMessageId } from "../../services/helper";
@@ -7,12 +7,16 @@ import { useAuth } from "../AuthContext";
 function UserCard({ onSelect, user }) {
   const { currentUser } = useAuth();
   const [lastMsg, setLastMsg] = useState(" ");
+  const [unread, setUnread] = useState(false);
 
   useEffect(() => {
     const msgId = getMessageId(currentUser, user);
     const unsub = onSnapshot(doc(db, "lastMsgs", msgId), (doc) => {
       if (doc.data()) {
         setLastMsg(doc.data().text);
+        if (doc.data().from !== currentUser.uid) {
+          setUnread(doc.data().unread);
+        }
       }
     });
 
@@ -30,7 +34,9 @@ function UserCard({ onSelect, user }) {
           {/* <p className="w-2 h-2 rounded-full bg-green-500"></p> */}
           <p className="w-2 h-2 rounded-full bg-red-500"></p>
         </div>
-        <p className="text-sm text-gray-600">{lastMsg}</p>
+        <p className="text-sm text-gray-600">
+          {unread ? <b>{lastMsg}</b> : <p>{lastMsg}</p>}
+        </p>
       </div>
     </div>
   );
