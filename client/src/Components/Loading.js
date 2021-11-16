e49144038f4f63bc2bc681c3f93138f3fc1d5db2
nitika2000 <nitikagupta208@gmail.com>
2021-11-17 01:59:51 +0530

    loader added

--- client/src/Components/Loading.js
+++ client/src/Components/Loading.js
@@ -1,7 +1,14 @@
 import React from "react";
+import ReactLoading from "react-loading";
 
 function Loading() {
-  return <div>Loading</div>;
+  return (
+    <div class="flex h-screen">
+      <div class="m-auto">
+        <ReactLoading type="bars" color="#000" height={50} width={50} />
+      </div>
+    </div>
+  );
 }
 
 export default Loading;
