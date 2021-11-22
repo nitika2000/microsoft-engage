c7eba8aeaffe995737f9beeddca78bea7b2a22cc
nitika2000 <nitikagupta208@gmail.com>
2021-11-22 16:49:00 +0530

    bug-fix in auth

--- client/src/Components/RequireAuth.js
+++ client/src/Components/RequireAuth.js
@@ -3,11 +3,14 @@ import { Navigate } from "react-router";
 
 export function RequireAuth({ children, pathType }) {
   let { currentUser } = useAuth();
+  if (pathType === "signUp" || pathType === "login") {
+    if (currentUser) return <Navigate to="/" />;
+    else {
+      return children;
+    }
+  }
   if (!currentUser) {
     return <Navigate to="/login" />;
-  }
-  if (currentUser && (pathType === "signUp" || pathType === "login")) {
-    return <Navigate to="/" />;
   }
   return children;
 }
