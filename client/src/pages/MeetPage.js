2483c4c0a04aa4d15c6cb3dfb7c4e94e6148f7b4
nitika2000 <nitikagupta208@gmail.com>
2021-11-27 18:06:24 +0530

    meet page ui updated

--- client/src/pages/MeetPage.js
+++ client/src/pages/MeetPage.js
@@ -11,27 +11,24 @@ const MeetPage = () => {
   const { currentUser } = useAuth();
 
   return (
-    <div>
-      <form>
-        <input
-          type="text"
-          className="form-input"
-          value={id}
-          onChange={(e) => setId(e.target.value)}
-        />
-        <button type="button" onClick={() => callUser(id)}>
-          Call
-        </button>
-        <p className="font-bold text-2xl">{currentUser.uid}</p>
-      </form>
-      <div className="flex flex-row mx-auto w-full">
-        <Video stream={stream} muted={true} />
-        <Video stream={remoteStream} />
+    <div className="bg-gray-200">
+      <div className="flex bg-gray-200 flex-row justify-center m-auto">
+        <div className="flex=1 h-full p-4">
+          <Video stream={stream} muted={true} />
+        </div>
+        <div className="flex=1 p-4 h-full">
+          <Video stream={remoteStream} />
+        </div>
       </div>
-      <p>Call id {callId}</p>
-      <button className="px-4 py-2 bg-red-500" onClick={endCall}>
-        End
-      </button>
+      <div className="flex ">
+        <button
+          onClick={endCall}
+          className="bg-red-500 m-auto font-semibold text-white py-2 px-4 border-transparent rounded"
+          type="button"
+        >
+          Close
+        </button>
+      </div>
     </div>
   );
 };
